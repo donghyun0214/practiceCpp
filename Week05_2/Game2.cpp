4679cbc4ff47ac8937e78de838ed04c576be7139
Donghyun <dev.donghyun0214@gmail.com>
2025-02-24 01:27:42 +0900

    Week07 Commit

--- Week05_2/Game2.cpp
+++ Week05_2/Game2.cpp
@@ -1,3 +1,24 @@
-//
-// Created by 김동현 on 2024. 12. 14..
-//
+#include <iostream>
+
+using namespace std;
+
+class Player {
+public:
+    Player(int hp, int xp)
+    : hp{ hp }, xp{ xp } {
+        cout << "생성자 호출됨" << endl;
+    }
+
+    void Print() {
+        cout << hp << " " << xp << endl;
+    }
+
+private:
+    int hp;
+    int xp;
+};
+
+int main() {
+    Player p{10, 2};
+    return 0;
+}
